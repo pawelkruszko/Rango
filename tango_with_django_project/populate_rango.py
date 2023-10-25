47699def13ad86c0388ff1f48164bcf94008cd4d
Paweł Kruszko <pawelkruszko@gmail.com>
2023-10-25 19:10:23 +0200

    populate script refactor

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -46,14 +46,16 @@ def populate():
 
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
+    p.url = url
+    p.views = views
     p.save()
     return p
 
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
