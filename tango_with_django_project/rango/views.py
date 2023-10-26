d3e69f7ea048d2c23cbf318e5ca6a7af700736ae
Paweł Kruszko <pawelkruszko@gmail.com>
2023-10-26 20:03:57 +0200

    index view modified

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rango.models import Category
 
 
 def index(request):
     context_dict = {
         'aboldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'
     }
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
     return render(request, 'rango/about.html', context={})
