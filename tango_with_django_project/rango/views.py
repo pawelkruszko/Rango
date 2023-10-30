7d21e0b24d57852b068010f73db537ecef4ca7b3
Paweł Kruszko <pawelkruszko@gmail.com>
2023-10-30 12:40:08 +0100

    populate script update

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 
 
 def index(request):
@@ -13,3 +13,18 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html', context={})
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
